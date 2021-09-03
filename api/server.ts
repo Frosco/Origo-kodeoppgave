6e7e83669e63bb59952fa213f130afbe92e9a2e0
Nils Grashorn <nils.grashorn@gmail.com>
2021-09-03 14:00:31 +0200

    Added calls to bysykkel

--- api/server.ts
+++ api/server.ts
@@ -1,11 +1,27 @@
+import got from 'got';
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const stationUrl = 'https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json';
+const statusUrl = 'https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json'
+const httpOptions = {
+  headers: {
+    'Client-Identifier': 'grashorn-kodeoppgave'
+  }
+};
+
+app.get('/stations', async (req, res) => {
+  const {data} = await got.get(stationUrl, httpOptions).json();
+  res.send(data);
+})
+
+app.get('/status', async (req, res) => {
+  const {data} = await got.get(statusUrl, httpOptions).json();
+  res.send(data);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`API listening at http://localhost:${port}`)
 })
